c90379a788c017923f9b2fc1219437de93606271
KrishnaP7 <spaka001@ucr.edu>
2018-10-26 15:53:08 -0700

    Added document model for photo upload

--- root/src/ilonely/pages/models.py
+++ root/src/ilonely/pages/models.py
@@ -56,3 +56,9 @@ class Message(models.Model):
 
     def __str__(self):
         return '%s message %s' % (self.author.get_username(), self.datePosted.strftime("%x %X"))
+
+class Document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
