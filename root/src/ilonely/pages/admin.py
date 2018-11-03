f297f59d0b6ee736626bcefe61deda9a98ef7835
Alex Tran <atran049@ucr.edu>
2018-11-02 18:42:46 -0700

    oops2

--- root/src/ilonely/pages/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from pages.models import Profile, Post, Follow, Block, Thread, Message
-
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(Follow)
-admin.site.register(Block)
-admin.site.register(Thread)
-admin.site.register(Message)
--- root/src/ilonely/pages/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class pagesConfig(AppConfig):
-    name = 'pages'
-
-    def ready(self):
-        import pages.signals
